98d5ea450d17bb5cece10bc11e3e81852f00dac5
crazab0i <145287199+crazab0i@users.noreply.github.com>
2024-12-19 16:56:59 -0800

    ground work for database and user storage

--- vaxchat/openaiChatBotTest/openaiChatbot/chatbot/views.py
+++ vaxchat/openaiChatBotTest/openaiChatbot/chatbot/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import os
+from dotenv import load_dotenv
+from openai import OpenAI
+load_dotenv("openai.env")
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_KEY")
+def ask_openai(message):
+    client = OpenAI(
+        api_key=os.environ.get("OPENAI_API_KEY")
+    ) 
+    response = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": message,
+            }
+        ],
+        model = 'gpt-3.5-turbo',
+        temperature=0.7,
+        max_tokens=150,
+        n=1,
+    )
+    return response.choices[0].message.content
 
-openai_api_key = os.environ
 # Create your views here.
 def chatbot(request): 
     if request.method == 'POST':
         message = request.POST.get('message')
-        response = 'Hi this is my response'
+        response = ask_openai(message)
         return JsonResponse({'message': message, 'response': response})
     return render(request, 'chatbot.html')
